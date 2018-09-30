7ff539ea218ac0e94c4b5bcd4b2cdb57330c1c52
ferdi <ferdian.agustinus@gmail.com>
2018-09-30 17:04:33 +0700

    Tambah Drawer

--- app/src/main/java/com/example/ferdi/penjualanamt/MainActivity.kt
+++ app/src/main/java/com/example/ferdi/penjualanamt/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.ferdi.penjualanamt
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.ferdi.penjualanamt.R.layout.activity_register
+import com.example.ferdi.penjualanamt.R.layout.activity_home
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun registerClicked(v : View){
+        val i = Intent(this, register::class.java )
+        startActivity(i)
+    }
+
+    fun loginClicked(v: View){
+        val i = Intent(this, drawer_navi::class.java)
+        startActivity(i)
     }
 }
---
+++ app/src/main/java/com/example/ferdi/penjualanamt/home.kt
@@ -0,0 +1,32 @@
+package com.example.ferdi.penjualanamt
+
+import android.os.Bundle
+import android.app.Activity
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import com.example.ferdi.penjualanamt.R.id.toolbar
+
+import kotlinx.android.synthetic.main.activity_home.*
+
+class home : AppCompatActivity()  {
+
+//    private val mDrawerLayout?: DrawerLayout
+//    private val mToggle? : ActionBarDrawerToggle
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_home)
+//
+//        mToggle = ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close)
+//
+//        mDrawerLayout.addDrawerListener(mToggle)
+//        mToggle.syncState()
+//
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true)
+
+    }
+
+}
